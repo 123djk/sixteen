2a5da43f3d0017d41bdeca132520f125d3ec2cef
Kirin72 <chubbybud1972@gmail.com>
2025-08-22 18:35:26 +0900

    Kirin commit2

--- monty_hall.py
+++ monty_hall.py
@@ -1,42 +1,4 @@
-#---Choi--
-
-import random
- #1 represents a car
- #0 represent a goat
-
-stay = 0  #amount won if stay in the same position
-switch = 0 # amount won if you switch 
-
-#----Lim---
-
-for i in range(1000):
-    lst = [1,0,0]           # one car and two goats
-    random.shuffle(lst)     # shuffles the list randomly
-    
-    ran = random.randrange(3) # gets a random number for the random guess
-
-    user = lst[ran] #storing the random guess 
-
-    del(lst[ran]) # deleting the random guess
-
-    huh = 0
-
-#----Soyong---    
-    
-    for i in lst: # getting a value 0 and deleting it
-        if i ==0:
-            del(lst[huh]) # deletes a goat when it finds it
-            break
-        huh+=1
-        
-    if user ==1: # if the original choice is 1 then stay adds 1
-        stay+=1
-        
-    if lst[0] == 1: # if the switched value is 1 then switch adds 1
-        switch+=1
-
-#----Jong---
 
 print("Stay =",stay)
 print("Switch = ",switch)
-print("Total = 1000):
+print("Total = 100,000"):
