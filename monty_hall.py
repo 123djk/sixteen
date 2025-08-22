286a67b62bcd118777af5b7940a12e405ef3e827
yourhotb <yourhotb@gmail.com>
2025-08-22 18:30:46 +0900

    feat: for i inst

---
+++ monty_hall.py
@@ -0,0 +1,12 @@
+    for i in lst: # getting a value 0 and deleting it
+        if i ==0:
+            del(lst[huh]) # deletes a goat when it finds it
+            break
+        huh+=1
+        
+    if user ==1: # if the original choice is 1 then stay adds 1
+        stay+=1
+        
+    if lst[0] == 1: # if the switched value is 1 then switch adds 1
+        switch+=1
+
